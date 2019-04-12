0e1c8514bac5a269c7d470e2fc29fb93d5b6d957
Arthur <teixeirarthur01@hotmail.com>
2019-04-11 21:30:03 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,69 @@
 #include <iostream>
+
 using namespace std;
 
-struct SalarioBase
+class Janela
 {
-    short salario;
+protected:
+    int var;
+
+public:
+    bool temBorda = true;
+    int corDeFundo = 0;
+
+    // Chamando a outra construtora que inicializa diretamente
+        // Mas nao precisa dessa porque se pode usar in-class initialization
+
+   // Janela () : Janela (true,0) {}
+    Janela() = default;
+
+    // Inicializacao e nao atribuicao
+    Janela (bool tB ,int cF ) : temBorda (tB) , corDeFundo (cF){}
+
+    void exibe () {}
+    void exibe (int a) {}
+
 };
 
-// "virtual" geralmente nao eh tao bom porque fica mais lento
-    // e funciona somente se todos usarem virtual
-struct Descontos : virtual public SalarioBase
+class MinhaJanela : public Janela
 {
-    short desconto;
-};
 
-struct Proventos : virtual public SalarioBase
-{
-    short proventos;
-};
+public:
+    bool temSombra;
 
-// A melhor solucao aqui era deixar de herdar nas outras structs e jogar tudo em "Folha"
-struct Folha : public Descontos, public Proventos
-{
-    short folha;
-};
+    MinhaJanela() : Janela (200, true) // Lista de inicializacao
+    {
+        var = 5; // Variavel protegida pode ser acessada pela heranca
+        temSombra = false;
+    }
 
+    using Janela::Janela; // Herdando construtoras da classe base
+    // Como ele vai usar a construtora da Base, nao vai chamar a construtora da derivada
+    // Logo, a inicializao in-class ha de ser utilizada
+
+    using Janela::exibe; // vai esconder as outras com mesmo nome a nao ser que use isso
+    void exibe ()
+    {
+        Janela::exibe(); // Herda o codigo da Base p
+                         // para nao ter que reescrever tudo novamente
+        // Resto de codigo para adicionar outras coisas na funcao exibe (como sombra)
+    }
+
+};
 
 int main()
 {
-    Folha f;
-    f.desconto = 1000;
-    f.proventos = 1200;
-    f.salario = 3000;
-    f.folha = f.salario + f.proventos - f.desconto;
+    Janela j;
+    j.corDeFundo = 250;
+    // j.var = 3;  esta protegido -> nao tem como acessar
+
+    MinhaJanela mj;
+    mj.temBorda = false;
+     // j.var = 7;  tambem esta protegido pois eh herdado protegido tambem
+
+    MinhaJanela nmj (200, false);
+    // Como essa construtora nao existe na classe derivada, vai chamar a da base
+    // assim como explicado na derivada
 
     return 0;
 }
